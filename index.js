68d41eef054e9a96124083b8ff0d08c3c90e09eb
D1360-64RC14 <gdiego416@gmail.com>
2020-06-05 20:29:54 -0300

    Hello World

---
+++ index.js
@@ -0,0 +1,180 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var jimp_1 = __importDefault(require("jimp"));
+var LEDPanel = (function () {
+    function LEDPanel(width, height) {
+        this.textPixelLength = 13;
+        this.backgroundColor = [0, 0, 0];
+        this.backgroundBright = 128;
+        this.textColor = [255, 255, 255];
+        this.width = width;
+        this.height = height;
+        this.queue = ['Started'];
+    }
+    ;
+    LEDPanel.prototype.printFrame = function (byteList) {
+        var _this = this;
+        if (byteList.length != this.width * this.height * 4) {
+            throw new RangeError("A entrada n\u00E3o coincide com o tamanho da imagem");
+        }
+        var pixelList = [];
+        var counter = 0;
+        var tempValue = [];
+        byteList.forEach(function (value) {
+            if (counter > 3) {
+                counter = 0;
+                pixelList.push(tempValue);
+                tempValue = [];
+            }
+            tempValue.push(value);
+            counter++;
+        });
+        pixelList.forEach(function () {
+            var line = pixelList.splice(0, _this.width);
+            var lineToPrint = line.map(function (v) {
+                return v.toString() == [255, 255, 255, 255].toString() ? 'B' : '.';
+            });
+            console.log(lineToPrint.join(' '));
+        });
+    };
+    ;
+    LEDPanel.prototype.createImage = function (width, height, bgColor, bgBright) {
+        var w = width ? width : this.width;
+        var h = height ? height : this.height;
+        var bg = bgColor ? bgColor : this.backgroundColor;
+        var bgB = bgBright ? bgBright : this.backgroundBright;
+        return Buffer.alloc(w * h * 4, bg[0])
+            .map(function (v, i) {
+            return (i + 1) % 2 == 0 ? bg[1] : v;
+        })
+            .map(function (v, i) {
+            return (i + 1) % 3 == 0 ? bg[2] : v;
+        })
+            .map(function (v, i) {
+            return (i + 1) % 4 == 0 ? bgB : v;
+        });
+    };
+    LEDPanel.prototype.writePixel = function (coords, color, data) {
+        var dataBuffer = data;
+        var line = this.width * coords[1];
+        var pixel = line + coords[0];
+        var offset = pixel * 4;
+        color.forEach(function (value, index) {
+            dataBuffer.writeUInt8(value, offset + index);
+        });
+        return dataBuffer;
+    };
+    LEDPanel.prototype.playAnimation = function (text) {
+        var _this = this;
+        return new Promise(function (resolv) {
+            var image = new jimp_1.default(_this.width, _this.height);
+            jimp_1.default.loadFont('./RobotoMono/RobotoMono.fnt', function (err, font) { return __awaiter(_this, void 0, void 0, function () {
+                var textPosition;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            textPosition = this.width;
+                            if (!(text.length * this.textPixelLength > this.width)) return [3, 5];
+                            textPosition;
+                            _a.label = 1;
+                        case 1:
+                            if (!(textPosition > this.width - (text.length * this.textPixelLength) - this.width)) return [3, 4];
+                            console.log();
+                            console.log();
+                            image.bitmap.data = this.createImage();
+                            image.print(font, textPosition, -5, text);
+                            panel.printFrame(image.bitmap.data);
+                            return [4, new Promise(function (r) { setTimeout(r, 50); })];
+                        case 2:
+                            _a.sent();
+                            _a.label = 3;
+                        case 3:
+                            textPosition--;
+                            return [3, 1];
+                        case 4: return [3, 7];
+                        case 5:
+                            image.bitmap.data = this.createImage();
+                            image.print(font, 0, -5, text);
+                            panel.printFrame(image.bitmap.data);
+                            return [4, new Promise(function (r) { setTimeout(r, 3000); })];
+                        case 6:
+                            _a.sent();
+                            _a.label = 7;
+                        case 7:
+                            resolv();
+                            return [2];
+                    }
+                });
+            }); });
+        });
+    };
+    LEDPanel.prototype.playQueue = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var phase;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!true) return [3, 2];
+                        phase = this.queue[0];
+                        if (this.queue.length > 1) {
+                            this.queue.shift();
+                        }
+                        return [4, this.playAnimation(phase)];
+                    case 1:
+                        _a.sent();
+                        return [3, 0];
+                    case 2: return [2];
+                }
+            });
+        });
+    };
+    LEDPanel.prototype.add2Queue = function (phase) {
+        this.queue.push(phase);
+    };
+    return LEDPanel;
+}());
+var panel = new LEDPanel(40, 24);
+panel.playQueue();
+panel.queue.push('Olá Mundo');
+panel.queue.push('Hello');
+panel.queue.push('AYAYA');
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,132 @@
+import Jimp from 'jimp';
+
+type Uint8 = 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|101|102|103|104|105|106|107|108|109|110|111|112|113|114|115|116|117|118|119|120|121|122|123|124|125|126|127|128|129|130|131|132|133|134|135|136|137|138|139|140|141|142|143|144|145|146|147|148|149|150|151|152|153|154|155|156|157|158|159|160|161|162|163|164|165|166|167|168|169|170|171|172|173|174|175|176|177|178|179|180|181|182|183|184|185|186|187|188|189|190|191|192|193|194|195|196|197|198|199|200|201|202|203|204|205|206|207|208|209|210|211|212|213|214|215|216|217|218|219|220|221|222|223|224|225|226|227|228|229|230|231|232|233|234|235|236|237|238|239|240|241|242|243|244|245|246|247|248|249|250|251|252|253|254|255;
+
+class LEDPanel {
+    width: number;
+    height: number;
+    queue: Array<string>;
+    textPixelLength = 13;
+    backgroundColor: [Uint8, Uint8, Uint8] = [0, 0, 0]; // Black
+    backgroundBright: Uint8 = 128;
+    textColor: [Uint8, Uint8, Uint8] = [255, 255, 255]; // White
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.queue = ['Started'];
+    };
+
+    printFrame(byteList: Buffer): void {
+        if(byteList.length != this.width * this.height * 4) {
+            throw new RangeError(`A entrada não coincide com o tamanho da imagem`);
+        }
+
+        let pixelList: Array<Array<number>> = [];
+        let counter = 0;
+        let tempValue: Array<number> = [];
+        byteList.forEach((value) => {
+            if(counter > 3){
+                counter = 0;
+                pixelList.push(tempValue);
+                tempValue = [];
+            }
+            tempValue.push(value);
+            counter++;
+        });
+
+        pixelList.forEach(() => {
+            let line = pixelList.splice(0, this.width);
+            let lineToPrint = line.map(v => {
+                return v.toString() == [255,255,255,255].toString() ? 'B' : '.';
+            });
+
+            console.log(lineToPrint.join(' '))
+        });
+    };
+
+    createImage(width?: number, height?: number, bgColor?: [Uint8, Uint8, Uint8], bgBright?: number): Buffer {
+        let w = width ? width : this.width;
+        let h = height ? height : this.height;
+        let bg = bgColor ? bgColor : this.backgroundColor;
+        let bgB = bgBright ? bgBright : this.backgroundBright;
+
+        // Muda todos os Alpha para 255
+        return <Buffer>Buffer.alloc(w * h * 4, bg[0]) // Set Color: RED
+        .map((v, i) => {
+            return (i + 1) % 2 == 0 ? bg[1] : v; // Set Color: GREEN
+        })
+        .map((v, i) => {
+            return (i + 1) % 3 == 0 ? bg[2] : v; // Set Color: BLUE
+        })
+        .map((v, i) => {
+            return (i + 1) % 4 == 0 ? bgB : v; // Set Color: ALPHA
+        });
+    }
+
+    writePixel(coords: [number, number], color: [number, number, number, number], data: Buffer){
+        let dataBuffer = data
+
+        let line = this.width * coords[1];
+        let pixel = line + coords[0];
+        let offset = pixel * 4;
+
+        color.forEach((value, index) => {
+            dataBuffer.writeUInt8(value, offset + index);
+        });
+
+        return dataBuffer;
+    }
+
+    playAnimation(text: string): Promise<void> {
+        return new Promise(resolv => {
+            let image = new Jimp(this.width, this.height);
+            
+            Jimp.loadFont('./RobotoMono/RobotoMono.fnt', async (err, font) => { // Font = 22
+                let textPosition = this.width;
+    
+                if(text.length * this.textPixelLength > this.width){
+                    for(textPosition; textPosition > this.width - (text.length * this.textPixelLength) - this.width; textPosition--){
+                        console.log()
+                        console.log()
+                        image.bitmap.data = this.createImage();
+                        image.print(font, textPosition, -5, text);
+        
+                        panel.printFrame(image.bitmap.data);
+        
+                        await new Promise(r => { setTimeout(r, 50) });
+                    }
+                } else {
+                    image.bitmap.data = this.createImage();
+                    image.print(font, 0, -5, text);
+                    panel.printFrame(image.bitmap.data);
+
+                    await new Promise(r => { setTimeout(r, 3000) });
+                }
+                resolv();
+            });
+        });
+    }
+
+    async playQueue(){
+        while(true){
+            let phase = this.queue[0];
+            if(this.queue.length > 1){
+                this.queue.shift();
+            }
+    
+            await this.playAnimation(phase)
+        }
+    }
+
+    add2Queue(phase: string): void {
+        this.queue.push(phase);
+    }
+}
+const panel = new LEDPanel(40, 24);
+
+panel.playQueue();
+
+panel.queue.push('Olá Mundo')
+panel.queue.push('Hello')
+panel.queue.push('AYAYA')
